7bc30764828fc7e6f667affe483b4754c39c5e7b
Mihail Ivanov <djzoning@gmail.com>
2019-12-13 13:47:20 +0200

    Edited and ready for production stage

--- handler.js
+++ handler.js
@@ -5,7 +5,8 @@ module.exports.hello = async event => {
     statusCode: 200,
     body: JSON.stringify(
       {
-        message: "Go Serverless v1.0! Your function executed successfully!"
+        message:
+          "Go Serverless v1.0! Your function executed successfully!. It's now edited for the production stage."
       },
       null,
       2
